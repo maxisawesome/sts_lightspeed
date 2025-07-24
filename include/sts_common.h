7a44f855f05eaef366387c5645a8a0a2a45d8ed1
Max Marion <mmarion538@gmail.com>
2025-07-24 13:03:41 -0700

    claude commit

--- include/sts_common.h
+++ include/sts_common.h
@@ -3,17 +3,34 @@
 //
 
 #ifndef STS_LIGHTSPEED_STS_COMMON_H
-#define STS_LIGHTSPEED_STS_COMMON_Hs
-//#define sts_print_debug
+#define STS_LIGHTSPEED_STS_COMMON_H
+// #define sts_print_debug
 #define sts_asserts
 
-//#define sts_action_queue_use_raw_array
-//#define sts_fixed_list_use_raw_array
-//#define sts_card_manager_use_fixed_list
-
+// #define sts_action_queue_use_raw_array
+// #define sts_fixed_list_use_raw_array
+// #define sts_card_manager_use_fixed_list
 
 #include <cstdint>
+#include <unordered_map>
 
+// First define the sts namespace and CardId enum
+namespace sts
+{
+    enum class CardId : uint16_t; // Forward declaration
+}
 
+// Then add hash support for CardId enum class
+namespace std
+{
+    template <>
+    struct hash<sts::CardId>
+    {
+        size_t operator()(const sts::CardId &id) const
+        {
+            return hash<uint16_t>()(static_cast<uint16_t>(id));
+        }
+    };
+}
 
-#endif //STS_LIGHTSPEED_STS_COMMON_H
+#endif // STS_LIGHTSPEED_STS_COMMON_H
